1a2ebfe33655b786a6f10998c973ce202551271e
pt2 <petro.domkin.25@kse.org.ua>
2026-07-02 16:00:25 +0300

    task 1.1

---
+++ task1.py
@@ -0,0 +1,11 @@
+book_profile = {"title": "Основи програмування",
+                "author": "Іван Петренко","year": 2023,
+                "publisher_info": {"name": "Наукова думка",
+                                   "city": "Київ",}
+                }
+
+name = book_profile["title"]
+author = book_profile["author"]
+
+print(f"Bookname is : {name}")
+print(f"The author is : {author}")
